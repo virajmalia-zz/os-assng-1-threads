a4dec74afe4d1c4e79b31a4a9cd77a9900facb80
Rachit R. Shah <rrs135@cp.cs.rutgers.edu>
2017-10-14 13:36:58 -0400

    my_pthread API created

--- my_pthread.c
+++ my_pthread.c
@@ -7,24 +7,204 @@
 // iLab Server:
 
 #include "my_pthread_t.h"
+#include <malloc.h>
+#include <sys/time.h>
+#include <signal.h>
+#define STACKSIZE 8*1024
+#define MAXTHREADS 20
+
+typedef struct {
+  my_pthread_t thread_id;
+  ucontext_t thread_context;
+  int active;
+  int executed;
+  int exited;
+  int isMain;
+  void *ret;
+}thread;
+
+static int threadid;
+extern ucontext_t common_context;
+struct sigaction scheduler_interrupt_handler;
+struct itimerval timeslice;
+sigset_t signalMask;
+void scheduler();
+void *helper(void *(*function)(void*), void *arg);
+thread_Queue queue = NULL;
+finished_Queue finishedQueue = NULL;
+tcb_ptr getCurrentControlBlock_Safe();
+long millisec;
+
+// init process
+void my_pthread_init(long period)
+{
+  threadid = 1;
+  sigemptyset(&signalMask);
+  sigaddset(&signalMask, SIGVTALRM);
+  //intializing the context of the scheduler
+  finishedQueue = getFinishedQueue();
+  queue = getQueue();
+  millisec = period;
+  tcb_ptr mainThread = getControlBlock_Main();
+  //getcontext(&(MainThread->thread_context));
+  printf("in init \n");
+  getCommonContext();
+  mainThread->thread_context.uc_link = &common_context;
+  mainThread->thread_id = threadid;
+  enqueue(queue,mainThread);
+  memset(&scheduler_interrupt_handler, 0, sizeof (scheduler_interrupt_handler));
+  scheduler_interrupt_handler.sa_handler= &scheduler;
+  sigaction(SIGVTALRM,&scheduler_interrupt_handler,NULL);
+  millisec = period;
+  timeslice.it_value.tv_sec = 0;
+  timeslice.it_interval.tv_sec = 0;
+  timeslice.it_value.tv_usec = millisec; // timer start decrementing from here to 0
+  timeslice.it_interval.tv_usec = millisec; //timer after 0 resets to this value
+  setitimer(ITIMER_VIRTUAL, &timeslice, NULL);
+  printf("Exiting init");
+}
+
 
 /* create a new thread */
 int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
-	return 0;
+  my_pthread_init(1000L);
+  int temp;
+  if(queue != NULL) {
+    sigprocmask(SIG_BLOCK,&signalMask,NULL);
+    tcb_ptr  threadCB= getControlBlock_Main();
+    getcontext((&threadCB->thread_context));
+    threadCB->thread_context.uc_stack.ss_sp=malloc(STACKSIZE);
+    threadCB->thread_context.uc_stack.ss_size=STACKSIZE;
+    threadCB->thread_context.uc_stack.ss_flags=0;
+    threadCB->isMain=0;
+    threadCB->thread_context.uc_link = &common_context;
+    //temp =rand();
+    threadCB->thread_id= ++threadid;
+    *thread = threadCB->thread_id;
+        
+    makecontext(&(threadCB->thread_context),(void (*)(void))&helper,2,start_routine,arg);
+    
+    printf("Thread is created %ld\n", *thread);
+    enqueue(queue,threadCB);
+    sigprocmask(SIG_UNBLOCK, &signalMask, NULL);
+    sigemptyset(&(threadCB->thread_context.uc_sigmask));
+    return 0;
+  }
+  printf("Error: init() function not executed/n");
+  return 0;
 };
+
+void *helper(void *(*start_routine)(void*), void *arg){
+
+  void *returnValue;
+  tcb_ptr currentThread = getCurrentControlBlock_Safe();
+  printf("In Helper");
+  returnValue = (*start_routine)(arg);
+  sigprocmask(SIG_BLOCK,&signalMask,NULL);
+  finishedThread_ptr finishedThread = getCompletedThread();
+  if(finishedThread != NULL) {  
+    *(finishedThread->returnValue) = returnValue;
+    finishedThread->thread_id = currentThread->thread_id;
+    enqueueToCompletedList(finishedQueue,finishedThread);
+  }
+  sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+  
+  return returnValue;
+  // set this value to the completed nodes return value
+}
+
+
+tcb_ptr getCurrentControlBlock_Safe() {
+
+  tcb_ptr currentControlBlock = NULL;
+  sigprocmask(SIG_BLOCK,&signalMask,null);
+  currentControlBlock = getCurrentBlock(queue);
+  sigprocmask(SIG_UNBLOCK,&signalMask,null);
+  
+  return currentControlBlock;
+
+}
 
 /* give CPU pocession to other user level threads voluntarily */
 int my_pthread_yield() {
-	return 0;
+  printf("\n-----Yield called-----\n");
+  raise(SIGVTALRM);
+  return 0;
 };
 
 /* terminate a thread */
 void my_pthread_exit(void *value_ptr) {
+  printf("\n-----Exit called-----\n");
+  sigprocmask(SIG_BLOCK,&signalMask,NULL);
+  tcb_ptr currentThread= getCurrentBlock(queue);
+  finishedThread_ptr finishedThread = getCompletedThread();
+  if(finishedThread !=NULL && currentThread != NULL) {
+    *(finishedThread->returnValue) = value_ptr;
+    finishedThread->thread_id = currentThread->thread_id;
+    enqueueToCompletedList(finishedQueue,finishedThread);
+  }
+  threadCompleted();
+  sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+  raise(SIGVTALRM);
 };
 
 /* wait for thread termination */
 int my_pthread_join(my_pthread_t thread, void **value_ptr) {
-	return 0;
+  //block signal
+  sigprocmask(SIG_BLOCK,&signalMask,NULL);
+  tcb_ptr callingThread = getCurrentBlock(queue);
+  tcb_ptr joinThread = getCurrentBlockByThread(queue,thread);
+  
+  //check if callingthread is blocking on itself or is null
+  if(callingThread ==NULL || callingThread == joinThread)
+    {       
+      //printf("\n 100.1 \n");
+      sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+      return -1;
+    }
+  if(joinThread==NULL)
+    {       //printf("\n 100.2 \n");
+      //The thread is finished hence can be found in finished Queue
+      FinishedThread_Ptr finishedThread = getFinishedThread(finishedQueue,thread,1);
+      sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+      if(finishedThread)
+	{  //printf("\n100.3\n");
+	  if(value_ptr)
+	    {//printf("\n 100.4 assigning value\n");
+	      *value_ptr =*(finishedThread->returnValue);
+	    }
+	  free(finishedThread);
+	  return 0;
+	}
+      else
+	return -1;
+      
+    }
+  
+  //printf("\n Value is %d :",(joinThread->blockedThreads==NULL));
+  if(joinThread->blockedThreads==NULL)
+    {
+      addToBlockedThreadList(joinThread,callingThread);
+      int isBlocked=callingThread->isBlocked;
+      sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+      while(isBlocked){
+	isBlocked=callingThread->isBlocked;
+      }
+      sigprocmask(SIG_BLOCK,&signalMask,NULL);
+      FinishedThread_Ptr finishedThread = getFinishedThread(finishedQueue,thread,1);
+      sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+      //printf("\n 100.51 \n");
+      if(finishedThread !=NULL && status !=NULL) {
+	if(status)
+	  *status=*(finishedThread->returnValue);
+	free(finishedThread);
+      }
+      return 0;
+    }
+  else {
+    sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+    return -1;
+  }
 };
 
 /* initial the mutex lock */
--- my_pthread_t.h
+++ my_pthread_t.h
@@ -16,24 +16,75 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ucontext.h>
+#define MAXTHREADS 20
 
 typedef uint my_pthread_t;
 
 typedef struct threadControlBlock {
 	/* add something here */
-} tcb; 
+  my_pthread_t thread_id;
+  ucontext_t thread_context;
+  int isActive;
+  int isExecuted;
+  int isBlocked;
+  int isMain;
+  struct threadControlBlock *next;
+  struct BlockedThreadList *blockedThreads;
+} tcb, *tcb_ptr; 
 
 /* mutex struct definition */
 typedef struct my_pthread_mutex_t {
 	/* add something here */
+  int lock;
+  int count;
+  volatile my_pthread_t owner;
 } my_pthread_mutex_t;
 
 /* define your data structures here: */
 
 // Feel free to add your own auxiliary data structures
+typedef struct threadQueue {
+  tcb_ptr head;
+  tcb_ptr tail;
+  long count;
+}*thread_Queue;
 
+typedef struct blockedThreadList {
+  tcb_ptr thread;
+  struct blockedThreadList *next;
+}*blockedThreadList_ptr;
+
+typedef struct finishedThread {
+  my_pthread_t thread_id;
+  void **returnValue;
+  struct finishedThread *next;
+}*finishedThread_ptr;
+
+typedef struct finishedControlBlockQueue {
+  struct finishedThread *thread;
+  long count;
+}*finishedQueue;
+
+tcb_ptr getControlBlock_Main();
+tcb_ptr getControlBlock();
+tcb_ptr getCurrentBlockByThread(thread_Queue,my_thread_t);
+tcb_ptr getCurrentBlock(thread_Queue queue);
+int getQueueSize(thread_Queue queue);
+thread_Queue getQueue();
+void freeControlBlock(tcb_ptr);
+int next(thread_Queue);
+int enqueueToCompletedList(finishedQueue,finishedThread_ptr);
+finishedThread_ptr getFinishedThread(finishedQueue,my_pthread_t,int);
+blockedThreadList_ptr getBlockedThreadList();
+int addToBlockedThreadList(tcb_ptr,tcb_ptr);
+finishedThread_ftr getCompletedThread();
+finishedQueue getFinishedQueue();
 
 /* Function Declarations: */
+
+// init process
+void my_pthread_init(long period);
 
 /* create a new thread */
 int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg);

b41c5a42ac5b936917995969d0f031406eda388b
Viraj Malia <virajmalia2@gmail.com>
2017-11-04 15:54:30 -0400

    Single Q to Heap transition started

--- my_pthread.c
+++ my_pthread.c
@@ -1,4 +1,4 @@
-// File:	my_pthread.c
+queue// File:	my_pthread.c
 // Author:	Yujie REN
 // Date:	09/23/2017
 
@@ -11,6 +11,7 @@
 #include <sys/time.h>
 #include <signal.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct {
   my_pthread_t thread_id;
@@ -22,6 +23,7 @@ typedef struct {
   void *ret;
 }thread;
 
+static bool first_create = true;
 static int threadid;
 extern ucontext_t common_context;
 struct sigaction scheduler_interrupt_handler;
@@ -34,9 +36,427 @@ finished_Queue finishedQueue = NULL;
 tcb_ptr getCurrentControlBlock_Safe();
 long millisec;
 
-// init process
-void my_pthread_init(long period)
-{
+/*
+Main Queue Implementation
+*/
+void max_heapify(tcb_ptr data, int loc, int count) {
+	int left, right, largest;
+    tcb temp;
+	left = 2*(loc) + 1;
+	right = left + 1;
+	largest = loc;
+
+
+	if ( left <= count && data[left].priority > data[largest].priority ) {
+		largest = left;
+	}
+	if ( right <= count && data[right].priority > data[largest].priority ) {
+		largest = right;
+	}
+
+    // Timestamps
+    clock_t now = clock();
+    clock_t parent_age = (now - data[largest].start_time) / CLOCKS_PER_SEC;
+
+    // If priority of parent == priority of left child
+    if ( left <= count && data[left].priority == data[largest].priority ) {
+		// timestamp will decide the largest
+        clock_t left_age = (now - data[left].start_time) / CLOCKS_PER_SEC;
+
+        if( left_age < parent_age ){
+            largest = left;
+        }
+	}
+
+    // If priority of parent == priority of right child
+    if ( right <= count && data[right].priority == data[largest].priority ) {
+		// timestamp will decide the largest
+        clock_t right_age = (now - data[right].start_time) / CLOCKS_PER_SEC;
+
+        if( right_age < parent_age ){
+            largest = right;
+        }
+    }
+
+	if(largest != loc) {
+		temp = data[loc];
+		data[loc] = data[largest];
+		data[largest] = temp;
+		max_heapify(data, largest, count);
+	}
+}
+
+int enqueue(thread_Queue queue, tcb_ptr value) {
+    int index, parent;
+    index = queue->count; // First insert at last of array
+
+    // Find out where to put the element and put it
+    while(index > 0){
+        parent = (index - 1) / 2;
+        if( queue->heaparr[parent]->priority > value->priority )
+                break;
+        if( queue->heaparr[parent]->priority == value->priority ){
+            // If priorities are same, check for timestamp
+            clock_t now = clock();
+            clock_t value_age = (now - value->start_time) / CLOCKS_PER_SEC;
+            clock_t parent_age = (now - queue->heaparr[parent]->start_time) / CLOCKS_PER_SEC;
+            //printf("value_age:%Lf\t parent_age:%Lf\n", (long double)value_age, (long double)parent_age );
+            // Newer thread at top
+            if( parent_age < value_age ){
+                // Insert value at this index
+                break;
+            }
+            // else keep moving value up the heap
+        }
+        queue->heaparr[index] = queue->heaparr[parent];
+        index = parent;
+    }
+
+    printf("Pushing:%d\n", value->thread_id);
+    queue->heaparr[index] = value;
+    queue->count++;
+
+    int i;
+    for(i=0; i < queue->count-1; i++){
+        queue->heaparr[i]->next = queue->heaparr[i+1];
+    }
+    queue->heaparr[i]->next = NULL;
+
+  return 0;
+}
+
+tcb_ptr dequeue(thread_Queue queue) {
+
+  if(queue == NULL)
+    return NULL;
+  else {
+    printf("\ndequeing blocks");
+    tcb_ptr removed;
+    queue->count--;
+    tcb_ptr last = queue->heaparr[queue->count];
+
+    // Last becomes top of heap; then max_heapify
+    removed = queue->heaparr[0];
+    queue->heaparr[0] = last;
+    max_heapify(queue->heaparr[0], 0, queue->count);
+
+    return removed;
+  }
+}
+
+int next(thread_Queue queue){
+  if(queue!= NULL) {
+      // next function takes the heap into the next state,
+      // where the top is popped and next thread is queued
+      tcb_ptr popped = dequeue(queue);
+
+      // Here, the popped thread has not completed execution,
+      // hence, add it to the back of the queue
+      queue->heaparr[queue->count] = popped;
+
+      // Reduce priority of popped thread before max_heapify
+      // so that it is not scheduled next
+      queue->heaparr[queue->count]->priority = 0;
+      max_heapify(queue->heaparr[0], 0, queue->count);
+  }
+  printf("\n Returning from next");
+  return 0;
+}
+
+int getQueueSize(thread_Queue queue) {
+  return queue->count;
+}
+
+thread_Queue getQueue() {
+  thread_Queue queue = (thread_Queue)malloc(sizeof(struct threadQueue)*MAXTHREADS);
+  queue->count = 0;
+  queue->size = MAXTHREADS;
+  return queue;
+}
+
+void heap_delete(thread_Queue h){
+    free(h);
+}
+/////////////////////////////////////
+
+/*
+Auxiliary Queue Implementation
+*/
+int enqueueToCompletedList(finished_Queue queue,finishedThread_ptr finishedThread ) {
+  if(queue != NULL && finishedThread !=NULL) {
+    finishedThread->next=queue->thread;
+    queue->thread = finishedThread;
+  }
+  return 0;
+}
+
+finishedThread_ptr getFinishedThread(finished_Queue queue,my_pthread_t thread_id,int flag) {
+
+  if(queue!=NULL) {
+    finishedThread_ptr thread= queue->thread;
+    finishedThread_ptr previous_thread = NULL;
+    while((thread!=NULL)&& (thread->thread_id!=thread_id)) {
+      previous_thread =thread;
+      thread = thread ->next;
+    }
+    if(flag && thread!=NULL) {
+      if(previous_thread == NULL)
+	     queue->thread  = thread->next;
+      else
+	     previous_thread->next = thread->next;
+    }
+    return thread;
+  }
+
+  return NULL;
+}
+
+blockedThreadList_ptr getBlockedThreadList() {
+
+  blockedThreadList_ptr newList = (blockedThreadList_ptr)malloc(sizeof(struct blockedThreadList));
+  if(newList!=NULL) {
+    newList->thread=NULL;
+    newList->next=NULL;
+  }
+  return newList;
+}
+
+int addToBlockedThreadList(tcb_ptr fromNode,tcb_ptr toNode ) {
+
+  blockedThreadList_ptr list = getBlockedThreadList();
+  if(fromNode != NULL) {
+    list->thread = toNode;
+    list->next = fromNode->blockedThreads;
+    fromNode->blockedThreads = list;
+    toNode->isBlocked=1;
+  }
+  return 0;
+}
+
+finishedThread_ptr getCompletedThread() {
+  finishedThread_ptr finishedThread = (finishedThread_ptr)malloc(sizeof(struct finishedThread));
+  if(finishedThread == NULL) {
+    return NULL;
+  }
+  finishedThread->returnValue=(void**)malloc(sizeof(void*));
+  if(finishedThread->returnValue ==NULL) {
+    free(finishedThread);
+    return NULL;
+  }
+  finishedThread->thread_id= -1;
+  *(finishedThread->returnValue)= NULL;
+  finishedThread->next =NULL;
+
+  return finishedThread;
+}
+
+finished_Queue getFinishedQueue() {
+  finished_Queue finishedQueue = (finished_Queue)malloc(sizeof(struct finishedControlBlockQueue));
+  finishedQueue->thread = NULL;
+  finishedQueue->count = 0;
+
+  return finishedQueue;
+}
+
+void threadCompleted() {
+
+  tcb_ptr currentNode = getCurrentControlBlock_Safe();
+  blockedThreadList_ptr blockedThread = currentNode->blockedThreads;
+
+  while(blockedThread != NULL){
+    blockedThread->thread->isBlocked = 0;
+    blockedThread = blockedThread->next;
+  }
+
+  printf("\n Thread completed : %d\n",currentNode->thread_id );
+  currentNode->isExecuted=1;
+
+  // Add thread to finishedQueue
+  //enqueueToCompletedList()
+
+  //removeFromHeap(currentNode->thread_id);
+  raise(SIGVTALRM);
+}
+
+////////////////////////////////////
+
+/*
+Scheduler
+*/
+void scheduler(int signum){
+    sigprocmask(SIG_BLOCK,&signalMask,NULL);
+    int q_size = getQueueSize(queue);
+    bool to_be_removed = 0;
+
+    if(q_size == 1){
+        if( getCurrentBlock(queue)->isExecuted ){
+            // If current context has finished execution, dequeue
+            dequeue(queue);
+        }
+    }
+    else if(q_size > 1){
+            tcb_ptr curr_context = getCurrentBlock(queue);
+            if( curr_context != NULL ){
+                if( curr_context->isExecuted ){
+                    to_be_removed = 1;
+                    // dequeue
+                    dequeue(queue);
+                }
+                else{
+                    next(queue);
+                }
+
+                tcb_ptr next_context = getCurrentBlock(queue);
+                while( next_context != NULL && ( next_context->isBlocked || next_context->isExecuted ) ){
+                    if( next_context->isExecuted ){
+                        // dequeue
+                        dequeue(queue);
+                    }
+                    else{
+                        next(queue);
+                    }
+                    next_context = getCurrentBlock(queue);
+                }
+
+                if( next_context == NULL ){
+                    sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+                    return;
+                }
+                if( next_context != curr_context ){
+                    if( to_be_removed ){
+                        // Set next thread as active, discard current thread
+                        setcontext( &(next_context->thread_context) );
+                    }
+                    else{
+                        // Swap current thread with next thread
+                        swapcontext(&(curr_context->thread_context), &(next_context->thread_context) );
+                    }
+                }
+            }
+        }
+        sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+}
+
+/////////////////////////////
+
+/*
+Context Functions
+*/
+void *helper(void *(*function)(void*), void *arg){
+
+  void *returnValue;
+  tcb_ptr currentThread = getCurrentControlBlock_Safe();
+  printf("In Helper");
+  returnValue = (*function)(arg);
+  sigprocmask(SIG_BLOCK,&signalMask,NULL);
+  finishedThread_ptr finishedThread = getCompletedThread();
+  if(finishedThread != NULL) {
+    *(finishedThread->returnValue) = returnValue;
+    finishedThread->thread_id = currentThread->thread_id;
+    enqueueToCompletedList(finishedQueue,finishedThread);
+  }
+  sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+
+  return returnValue;
+  // set this value to the completed nodes return value
+}
+
+tcb_ptr getCurrentControlBlock_Safe() {
+
+  tcb_ptr currentControlBlock = NULL;
+  sigprocmask(SIG_BLOCK,&signalMask,NULL);
+  currentControlBlock = getCurrentBlock(queue);
+  sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
+
+  return currentControlBlock;
+
+}
+
+tcb_ptr getControlBlock_Main(){
+  tcb_ptr controlBlock = (tcb_ptr)malloc(sizeof(tcb));
+  controlBlock->thread_context.uc_stack.ss_flags = 0;
+  controlBlock->thread_context.uc_link =0;
+  controlBlock->isActive =0;
+  controlBlock->isBlocked =0;
+  controlBlock->isExecuted =0;
+  controlBlock->isMain = 1 ;
+  controlBlock->priority = 4;
+  controlBlock->next = NULL ;
+
+  return controlBlock;
+
+}
+
+tcb_ptr getControlBlock(){
+  tcb_ptr controlBlock = (tcb_ptr)malloc(sizeof(tcb_ptr));
+  controlBlock->thread_context.uc_stack.ss_sp = malloc(STACKSIZE);
+  controlBlock->thread_context.uc_stack.ss_size = STACKSIZE;
+  controlBlock->thread_context.uc_stack.ss_flags = 0;
+  controlBlock->thread_context.uc_link =0;
+  controlBlock->isActive =0;
+  controlBlock->isBlocked =0;
+  controlBlock->isExecuted =0;
+  controlBlock->isMain =0 ;
+  controlBlock->priority = 4;
+  controlBlock->next = NULL ;
+
+  return controlBlock;
+
+}
+
+tcb_ptr getCurrentBlock(thread_Queue queue){
+  if(queue != NULL && getQueueSize(queue) != 0) {
+    printf("\n Returning CurrentBlock\n");
+    return queue->heaparr[0];
+  }
+  return NULL;
+}
+
+tcb_ptr getCurrentBlockByThread(thread_Queue queue,my_pthread_t threadid) {
+  tcb_ptr headBlock = getCurrentBlock(queue);
+  //if this is the required node
+  if(headBlock!=NULL && headBlock->thread_id == threadid)
+    return headBlock;
+  tcb_ptr dummyThread=NULL;
+  if(headBlock!=NULL)
+    dummyThread = headBlock->next;
+
+  while((headBlock != dummyThread)) {
+    if(dummyThread ->thread_id == threadid)
+      return dummyThread;
+
+    dummyThread = dummyThread->next;
+  }
+  return NULL;
+}
+
+void freeControlBlock(tcb_ptr controlBlock) {
+  if(!(controlBlock->isMain))
+    free(controlBlock->thread_context.uc_stack.ss_sp);
+
+  free(controlBlock);
+}
+
+ucontext_t getCommonContext() {
+  static int contextAlreadySet = 0;
+  if(!contextAlreadySet)
+  {
+    getcontext(&common_context);
+    common_context.uc_link = 0;
+    common_context.uc_stack.ss_sp = malloc(STACKSIZE);
+    common_context.uc_stack.ss_size = STACKSIZE;
+    common_context.uc_stack.ss_flags= 0;
+    makecontext( &common_context, (void (*) (void))&threadCompleted, 0);
+    contextAlreadySet = 1;
+  }
+}
+
+//////////////////////////////////////////////////
+
+/*
+Pthread API
+*/
+void my_pthread_init(long period){
   threadid = 1;
   sigemptyset(&signalMask);
   sigaddset(&signalMask, SIGVTALRM);
@@ -63,10 +483,13 @@ void my_pthread_init(long period)
   printf("Exiting init");
 }
 
-
 /* create a new thread */
 int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
-  my_pthread_init(1000L);
+  //my_pthread_init(1000L);
+  if(first_create){
+    first_create = false;
+    my_pthread_init(25000);
+    }
   int temp;
   if(queue != NULL) {
     sigprocmask(SIG_BLOCK,&signalMask,NULL);
@@ -92,37 +515,6 @@ int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*func
   printf("Error: init() function not executed/n");
   return 0;
 };
-
-void *helper(void *(*function)(void*), void *arg){
-
-  void *returnValue;
-  tcb_ptr currentThread = getCurrentControlBlock_Safe();
-  printf("In Helper");
-  returnValue = (*function)(arg);
-  sigprocmask(SIG_BLOCK,&signalMask,NULL);
-  finishedThread_ptr finishedThread = getCompletedThread();
-  if(finishedThread != NULL) {
-    *(finishedThread->returnValue) = returnValue;
-    finishedThread->thread_id = currentThread->thread_id;
-    enqueueToCompletedList(finishedQueue,finishedThread);
-  }
-  sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
-
-  return returnValue;
-  // set this value to the completed nodes return value
-}
-
-
-tcb_ptr getCurrentControlBlock_Safe() {
-
-  tcb_ptr currentControlBlock = NULL;
-  sigprocmask(SIG_BLOCK,&signalMask,NULL);
-  currentControlBlock = getCurrentBlock(queue);
-  sigprocmask(SIG_UNBLOCK,&signalMask,NULL);
-
-  return currentControlBlock;
-
-}
 
 /* give CPU pocession to other user level threads voluntarily */
 int my_pthread_yield() {
@@ -256,8 +648,9 @@ int my_pthread_mutex_unlock(my_pthread_mutex_t *mutex) {
 /* destroy the mutex */
 int my_pthread_mutex_destroy(my_pthread_mutex_t *mutex) {
   printf("Mutex destroy \n");
-  mutex->lock = NULL;
-  mutex->owner = NULL;
-  mutex->count = NULL;
-	return 0;
+  mutex->lock = -1;
+  mutex->owner = -1;
+  mutex->count = -1;
+  return 0;
 };
+///////////////////////////////////////////////////////
--- my_pthread_t.h
+++ my_pthread_t.h
@@ -10,10 +10,13 @@
 
 #define _GNU_SOURCE
 
-/* include lib header files that you need here: */
+#define USE_MY_PTHREAD 1
+
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <sys/time.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ucontext.h>
@@ -23,32 +26,29 @@
 typedef uint my_pthread_t;
 
 typedef struct threadControlBlock {
-	/* add something here */
   my_pthread_t thread_id;
   ucontext_t thread_context;
   int isActive;
   int isExecuted;
   int isBlocked;
   int isMain;
+  int priority;
+  clock_t start_time;
   struct threadControlBlock *next;
   struct blockedThreadList *blockedThreads;
 } tcb, *tcb_ptr;
 
 /* mutex struct definition */
 typedef struct my_pthread_mutex_t {
-	/* add something here */
   int lock;
   int count;
   volatile my_pthread_t owner;
 } my_pthread_mutex_t;
 
-/* define your data structures here: */
-
-// Feel free to add your own auxiliary data structures
 typedef struct threadQueue {
-  tcb_ptr head;
-  tcb_ptr tail;
-  long count;
+    int size;
+	int count;
+	tcb_ptr heaparr[MAXTHREADS];
 }*thread_Queue;
 
 typedef struct blockedThreadList {
@@ -110,5 +110,17 @@ int my_pthread_mutex_unlock(my_pthread_mutex_t *mutex);
 
 /* destroy the mutex */
 int my_pthread_mutex_destroy(my_pthread_mutex_t *mutex);
+
+#ifdef USE_MY_PTHREAD
+#define pthread_t my_pthread_t
+#define pthread_mutex_t my_pthread_mutex_t
+#define pthread_create my_pthread_create
+#define pthread_exit my_pthread_exit
+#define pthread_join my_pthread_join
+#define pthread_mutex_init my_pthread_mutex_init
+#define pthread_mutex_lock my_pthread_mutex_lock
+#define pthread_mutex_unlock my_pthread_mutex_unlock
+#define pthread_mutex_destroy my_pthread_mutex_destroy
+#endif
 
 #endif
